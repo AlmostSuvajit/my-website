b65e914eaca9d1855e7d873dd76530d176297c2c
Suvajit <suvajit.c@outlook.com>
2019-05-08 21:53:29 +0530

    fix://

--- public/website/clashperks/script/script.js
+++ public/website/clashperks/script/script.js
@@ -1,11 +1,11 @@
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "https://api.almostsuvajit.xyz/raven/", true);
+xmlhttp.open("GET", "https://api.almostsuvajit.xyz/clashperks/", true);
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
         document.getElementById("commands_used").innerHTML = myObj.commands_used;
         document.getElementById("messages_seen").innerHTML = myObj.messages_seen;
-        document.getElementById("free_memory").innerHTML = myObj.free_memory;
+        document.getElementById("clans_in_db").innerHTML = myObj.clans_in_db;
         document.getElementById("memory_usage").innerHTML = myObj.memory_usage;
         document.getElementById("commands").innerHTML = myObj.commands;
         document.getElementById("channels").innerHTML = myObj.channels;
